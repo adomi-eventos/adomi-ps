ff8e2dcd8b9ad1f09614124dd1b514c18fccf182
Elias Biondo <contato@eliasbiondo.com>
2021-08-18 16:35:27 -0300

    :sparkles: added the first exercise challenge solution :)

---
+++ exercises/1st/challenge.js
@@ -0,0 +1,23 @@
+/*
+ * Exercise description: 
+ *
+ * Given a list of integer numbers, print on screen if  
+ * its numbers are odd or even just using a single line.
+ *
+ */
+
+/**
+ * Main function of the exercise (self-invoked).
+ */
+(function main() {
+
+    // Instancing the "list" variable to storage all verification target numbers.
+    let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    // Printing on screen if each number is odd or even using just a single line.
+    list.forEach(number => { number % 2 == 0 ? console.log(`${number} is even!`) : console.log(`${number} is odd!`)})
+
+    // Returning standard machine-readable success answer.
+    return 0;
+
+})();
