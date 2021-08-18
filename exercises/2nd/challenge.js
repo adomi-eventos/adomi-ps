dc16eb02e68e105ca7e1f2f672899138b756b0f6
Elias Biondo <contato@eliasbiondo.com>
2021-08-18 17:18:20 -0300

    :sparkles: added the second exercise challenge solution :)

---
+++ exercises/2nd/challenge.js
@@ -0,0 +1,62 @@
+/**
+ * Exercise description: 
+ *
+ * Write an algorithm that storage the value "19" inside an "a"
+ * variable and the value "23" inside a "b" variable. Next, swi-
+ * pe their contents making the value inside "a" switch to "b"
+ * and vice-versa without use a third variable. At the end, pri-
+ * nt the values storaged in each variable.
+ *  
+ */
+
+// Defining the "Number" class with a value property. 
+class Number {
+    constructor(value) {
+        this.value = value;
+    }
+}
+
+/**
+ * Swipe "Number" objects values.
+ * @param first A "Number" object.
+ * @param second A "Number" object.
+ */
+function swipe(first, second) {
+
+    // Assigning the first object value as the first object value plus second object value.
+    first.value += second.value;
+
+    // Assigning the second object value as the first object value minus second object value.
+    second.value = first.value - second.value;
+
+    // Assigning the first object value as the first object value minus second object value.
+    first.value -= second.value;
+
+    // Returning void.
+    return;
+}
+
+/**
+ * Main function of the exercise (self-invoked).
+ */
+ (function main() {
+    
+    // Instancing the "a" variable as a "Number" object with the "19" value.
+    let a = new Number(19);
+
+    // Instancing the "b" variable as a "Number" object with the "23" value.
+    let b = new Number(23);
+
+    // Printing the values before swipe.
+    console.log(`a: ${a.value} b: ${b.value}`);
+
+    // Swiping the values by using the swipe() function.
+    swipe(a, b);
+
+    // Printing the values after swipe.
+    console.log(`a: ${a.value} b: ${b.value}`);
+
+    // Returning standard machine-readable success answer.
+    return 0;
+
+})();
