f0487b2faa04e991dc7ac5479c7339f00c525238
Marcos Paulo M Cabral <69735297+MarcosCabral93@users.noreply.github.com>
2021-08-20 00:31:23 -0300

    resolvendo o segundo  exercicio

---
+++ exercicio2.js
@@ -0,0 +1,19 @@
+function trocaNivelIni(){
+let a =19;
+let b=23;
+let c=0;
+c=a;
+a=b;
+b=c;
+console.log(a,b)}
+trocaNivelIni();
+
+function trocaNivelExp(){
+var a =19;
+var b=23;
+//passo dentro de um array reatribuindo valores
+[a,b]=[b,a]
+
+console.log(a,b)}
+
+trocaNivelExp();
