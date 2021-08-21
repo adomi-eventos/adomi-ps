bd0b08f71b12fb66f5205e0225e52dfad551c925
Caio <caio_f.alves@hotmail.com>
2021-08-21 00:02:26 -0300

    Resolução do exercício 1a

---
+++ 1a.js
@@ -0,0 +1,9 @@
+let lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i <= lista.length; i++) {
+  if (i % 2 == 0) {
+    console.log(`${i} é par`);
+  } else {
+    console.log(`${i} é ímpar`);
+  }
+}
